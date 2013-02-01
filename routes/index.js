8cba9ce95fa806de42a9b1922a3979ed3e024218
nubbie <irtehneeb@gmail.com>
2013-02-01 21:40:03 +1100

    added pagination, and loading message; rearranged columns

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,6 @@
 var http = require('http');
 var url = require('url');
 
-var options = {
-  host: 'api.reddit.com',
-  port: 80,
-  path: '/?limit=100',
-  method: 'GET',
-  headers: {
-    'User-Agent': 'spreaddit v0.00001 by /u/nubix'
-  }
-};
-
 exports.index = function(req, res){
-  // var content = "";
-  // var get = http.request(options, function(getRes) {
-  //   getRes.setEncoding('utf8');
-  //   getRes.on('data', function (chunk) {
-  //     content += chunk;
-  //   });
-  //   getRes.on('end', function(){
-      
-  //   });
-  // }).on('error', function(e) {
-  //   console.log('ERROR: ' + e.message);
-  // });
-  // get.end();
-  console.log(req);
-  var url_parts = url.parse(req.url, true);
-  var get_url = '/';
-  if (typeof url_parts.query.url != 'undefined'){
-    get_url = url_parts.query.url;
-  }
-  res.render('index', { purl: get_url });
-
+  res.render('index', { url: req.originalUrl });
 };
